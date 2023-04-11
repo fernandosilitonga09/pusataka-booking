f4e76ad72f4132d3c9571754f50e4532d729237f
fernandosilitonga09 <fernandosilitonga0908@gmail.com>
2023-04-11 18:10:36 +0700

    add new file

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4>
     <ul type="disc">
-        <li>Nama Depan : ?</li>
-        <li>Nama Belakang : ?</li>
+        <li>Nama Depan : Fernando </li>
+        <li>Nama Belakang : Silitonga</li>
     </ul>
     <br>
     <h4>Alamat</h4>
     <ul type="none">
-        <li> Jalan ?</li>
+        <li> Jalan ~</li>
     </ul>
 
     <h4>Tempat Lahir</h4>
@@ -17,7 +17,7 @@
     </ul>
     <h4>Olah Raga Favorit</h4>
     <ul type="square">
-        <li>?</li>
-        <li>?</li>
+        <li>~</li>
+        <li>~</li>
     </ul>
 </section>
